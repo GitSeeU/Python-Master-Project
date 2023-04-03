19a658aef6f3531fd66932851bdde0930e45beb0
GitSeeU <127952876+GitSeeU@users.noreply.github.com>
2023-04-03 19:16:04 +0900

    Add files via upload

---
+++ 2. blooean 자료형.py
@@ -0,0 +1,9 @@
+# 참 / 거짓 
+print(5 > 10) # print 는 출력을 위한 명령문이고 5는 10보다 작기때문에 flase를 출력한다.
+print(5 < 11) # print 는 출력을 위한 명령문이고 5는 11보다 작기 때문에 True를 출력한다.
+print(True) # print 는 출력을 위한 명령문이고 "True"를 출력한다.
+print(False) # print 는 출력을 위한 명령문이고 "False"를 출력한다.
+print(not True) # print 는 출력을 위한 명령문이고 not True 이기 때문에 "False"를 출력한다.
+print(not False) # print 는 출력을 위한 명령문이고 not True 이기 때문에 "True"를 출력한다.
+print( not(5 > 10)) # print 는 출력을 위한 명령문이고 not(False) 이기 때문에 "True"를 출력한다.
+print(not(6 < 10)) # print 는 출력을 위한 명령문이고 not(True) 이기 때문에 "False"를 출력한다.
